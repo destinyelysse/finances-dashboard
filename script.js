b7d2b59e9ab2187a415095f9602645e8510d1fb8
Destiny Serna <destinyeserna@gmail.com>
2026-05-10 09:34:21 -0700

    feat: Add action items functionality

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const noGoalsNote = document.getElementById("no-goals-note");
 const goalsList = document.getElementById("goals-list");
 const newGoalInput = document.getElementById("new-goal");
-const addGoalButton = document.getElementById("add-entered-goal-btn");
+const addGoalBtn = document.getElementById("add-entered-goal-btn");
 const editGoalsBtn = document.getElementById("btn-edit-goals");
+const goalInput = document.getElementById("input-goal");
+
+const noActionItemsNote = document.getElementById("no-action-items-note");
+const actionItemsList = document.getElementById("action-items-list");
+const newActionInput = document.getElementById("new-action-item");
+const addActionItemBtn = document.getElementById("add-entered-action-item-btn");
+const editActionItemsBtn = document.getElementById("btn-edit-action-items");
+const actionInput = document.getElementById("input-action-item");
 
 const goals = [];
-console.log("rerunning");
+const actionItems = [];
 
 function toggleEditGoals() {
-  const goalInput = document.getElementById("input-goal");
   goalInput.hidden = !goalInput.hidden;
   editGoalsBtn.textContent = goalInput.hidden ? "Edit Goals" : "Done";
 }
@@ -28,5 +35,30 @@ function addGoalToList() {
   newGoalInput.value = "";
 }
 
+function toggleEditActionItems() {
+  actionInput.hidden = !actionInput.hidden;
+  editActionItemsBtn.textContent = actionInput.hidden
+    ? "Edit Action Items"
+    : "Done";
+}
+
+function actionsListContentManager() {
+  noActionItemsNote.hidden = actionItems.length !== 0;
+  actionItemsList.hidden = actionItems.length == 0;
+}
+
+function addActionItemsToList() {
+  const newActionItemText = newActionInput.value;
+  actionItems.push(newActionItemText);
+  const newActionItemLi = document.createElement("li");
+  newActionItemLi.textContent = newActionItemText;
+  actionItemsList.append(newActionItemLi);
+  actionsListContentManager();
+  newActionInput.value = "";
+}
+
 editGoalsBtn.onclick = toggleEditGoals;
-addGoalButton.onclick = addGoalToList;
+addGoalBtn.onclick = addGoalToList;
+
+editActionItemsBtn.onclick = toggleEditActionItems;
+addActionItemBtn.onclick = addActionItemsToList;
